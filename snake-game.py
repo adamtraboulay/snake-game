effa023881452c8ee2dfe0f7ac41310fd64213ca
adamtraboulay <traboulayadam@gmail.com>
2022-04-06 11:42:58 -0400

    commit

--- snake-game.py
+++ snake-game.py
@@ -19,7 +19,7 @@ else:
 
 
 # Initialise game window
-pygame.display.set_caption('Snake Eater')
+pygame.display.set_caption("Adam's snake game")
 game_window = pygame.display.set_mode((frame_size_x, frame_size_y))
 
 
